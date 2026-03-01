90539bd362dd30875163f537a4b49ca1b18f1f71
ma10-r <madis.roosma@gmail.com>
2026-03-01 16:02:05 +0200

    olen siin

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Ommik! Muutsin midagi"));
+    IO.println(String.format("Ommik! Olen endiselt siin"));
 
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
